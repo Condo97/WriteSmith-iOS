c00a39d7fcf0ed0471fed536eac4b69e7d397a3b
Alex C <acoundou@gmail.com>
2023-04-04 15:50:44 -0400

    Better JSON handling

--- ChitChat/Controllers/Chat/Extensions/ChatViewController+CameraViewControllerDelegate.swift
+++ ChitChat/Controllers/Chat/Extensions/ChatViewController+CameraViewControllerDelegate.swift
@@ -9,36 +9,7 @@ import Foundation
 
 extension ChatViewController: CameraViewControllerDelegate {
     func didGetScan(text: String) {
-        guard let authToken = UserDefaults.standard.string(forKey: Constants.authTokenKey) else {
-            HTTPSHelper.registerUser(delegate: self)
-            return
-        }
-        
-        //Button is disabled until response for premium
-        //Button is enabled BUT shows a popup when pressed
-        if UserDefaults.standard.bool(forKey: Constants.userDefaultStoredIsPremium) {
-            submitButton.isEnabled = false
-            cameraButton.isEnabled = false
-        } else {
-            DispatchQueue.main.async {
-                self.submitButton.alpha = 0.8
-                self.cameraButton.alpha = 0.8
-                self.submitSoftDisable = true
-            }
-        }
-         
-        HTTPSHelper.getChat(delegate: self, authToken: authToken, inputText: text)
-        addChat(message: text, userSent: .user)
-        
-        if !isProcessingChat {
-            isProcessingChat = true
-            tableView.beginUpdates()
-            tableView.insertRows(at: [IndexPath(row: tableView.numberOfRows(inSection: 0), section: 0)], with: .automatic)
-            tableView.endUpdates()
-        }
-        
-        // Show Ad if Not Premium
-        loadGAD()
+        generateChat(inputText: text)
     }
 }
 
